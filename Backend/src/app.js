bc015a7a53c5ce3671fb93d9d3d49e2f67b6bb6d
BimalSinha59 <bimalsinha6264@gmail.com>
2025-12-30 00:17:56 +0530

    auth and database synchronization

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,24 +2,32 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
 
 dotenv.config({
     path: './.env',
-    quiet:true // does not show the env var information on terminal
+    quiet:true // does not show the env variable information on terminal
 })
 
 const app = express();
 
 const __dirname = path.resolve();
 
+// middlewares
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-
+    credentials: true //=> server allows a browser(frontend) to include cookies on request
+}));
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
+
+app.use("/api/inngest", serve({
+    client: inngest,
+    functions
+}));
 
 app.get("/health", (req,res) => {
     res.status(200).json({msg: "aoi is up and running"});
